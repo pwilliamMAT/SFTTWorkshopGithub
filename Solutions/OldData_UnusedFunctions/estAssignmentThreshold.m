94ecbd0db77493ad239f5af70d5367057574521c
Pat Williamson <pwilliam@mathworks.com>
2025-01-09 12:02:36 -0500

    remove folder with outdated code and data files

--- Solutions/OldData_UnusedFunctions/estAssignmentThreshold.m
+++
@@ -1,7 +0,0 @@
-function AssignmentThreshold = estAssignmentThreshold(gateMult,noisyDetections)
-% Gate input should be a multiplier on the measurementnoise - ie you expect
-% your assignmentThreshold should be approx 5x MeasurementNoise
-
-AssignmentThreshold = gateMult^2 + log(det(gateMult*noisyDetections(1).MeasurementNoise(1,1)));
-
-end
